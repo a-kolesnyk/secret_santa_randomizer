0193f5168832b02e2d63cdd54131cfa87ced1b1a
Oleksandr Kolesnyk <shepard.via.n@gmail.com>
2023-10-17 23:06:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+# app.py
+
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/generate_pairs", methods=["POST"])
 def generate_pairs():
-    names = request.form.getlist("names")
+  names = request.form.getlist("name")
 
-    random.shuffle(names)
-    pairs = [(names[i], names[(i + 1) % len(names)]) for i in range(len(names))]
+  random.shuffle(names)
+  pairs = [(names[i], names[(i + 1) % len(names)]) for i in range(len(names))]
 
-    return render_template("pairs.html", pairs=pairs)
+  return render_template("pairs.html", pairs=pairs)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
